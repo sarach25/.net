bde3fd6bcf62c53b45ce9480e48b69a208a64542
ch sara <sara.chebbi@esprit.tn>
2017-07-21 11:50:52 +0200

    web +test4

--- UnitTestProject1/UnitTest1.cs
+++ UnitTestProject1/UnitTest1.cs
@@ -38,5 +38,25 @@ namespace UnitTestProject1
             //Assert : verifier le output 
             Assert.AreEqual(0, resultat);
         }
+
+
+        [TestMethod]
+
+        public void TestMethod4()
+        {
+
+            // les 3 a 
+            // Arrange  : ntsutuw el clqss vqlues controller
+
+            var contrller = new ValuesController();
+
+            //Act 
+            var resultat = contrller.multi(4, 2);
+
+            //Assert : verifier le output 
+            Assert.AreEqual(1, resultat);
+        }
+
+
     }
 }
